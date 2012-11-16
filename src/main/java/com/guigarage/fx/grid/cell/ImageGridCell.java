2915c0b04d78bb6bd9c0f132e8304cb06a1e4514
Hendrik Ebbers <hendrik.ebbers@web.de>
2012-11-16 18:11:37 +0100

    default cells for image & media

---
+++ src/main/java/com/guigarage/fx/grid/cell/ImageGridCell.java
@@ -0,0 +1,32 @@
+package com.guigarage.fx.grid.cell;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.ImageViewBuilder;
+
+import com.guigarage.fx.grid.GridCell;
+
+public class ImageGridCell extends GridCell<Image> {
+	
+	public ImageGridCell() {
+		getStyleClass().add("image-grid-cell");
+		//TODO: die Basis-Cells sollten einen eigenen Eintrag im CSS bekommen und nicht das setCssDependency nutzen
+//		setCssDependency();
+		itemProperty().addListener(new ChangeListener<Image>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Image> arg0,
+					Image arg1, Image arg2) {
+				getChildren().clear();
+				//TODO: Die Properties des ImageView sollten ins CSS wandern
+				ImageView imageView = ImageViewBuilder.create().image(arg2).build();
+				imageView.fitHeightProperty().bind(heightProperty());
+				imageView.fitWidthProperty().bind(widthProperty());
+				
+				setGraphic(imageView);
+			}
+		});
+	}
+}
---
+++ src/main/java/com/guigarage/fx/grid/cell/MediaImageCell.java
@@ -0,0 +1,56 @@
+package com.guigarage.fx.grid.cell;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaPlayerBuilder;
+import javafx.scene.media.MediaView;
+import javafx.scene.media.MediaViewBuilder;
+
+import com.guigarage.fx.grid.GridCell;
+
+public class MediaImageCell extends GridCell<Media> {
+	
+	private MediaPlayer mediaPlayer;
+	
+	public MediaImageCell() {
+		getStyleClass().add("media-grid-cell");
+		//TODO: die Basis-Cells sollten einen eigenen Eintrag im CSS bekommen und nicht das setCssDependency nutzen
+//		setCssDependency();
+		itemProperty().addListener(new ChangeListener<Media>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Media> arg0,
+					Media arg1, Media arg2) {
+				getChildren().clear();
+				if(mediaPlayer != null) {
+					mediaPlayer.stop();
+				}
+				mediaPlayer = MediaPlayerBuilder.create().media(arg2).build();
+				MediaView mediaView = MediaViewBuilder.create().mediaPlayer(mediaPlayer).build();
+				mediaView.fitHeightProperty().bind(heightProperty());
+				mediaView.fitWidthProperty().bind(widthProperty());
+				setGraphic(mediaView);
+			}
+		});
+	}
+	
+	public void pause() {
+		if(mediaPlayer != null) {
+			mediaPlayer.pause();
+		}
+	}
+	
+	public void play() {
+		if(mediaPlayer != null) {
+			mediaPlayer.play();
+		}
+	}
+	
+	public void stop() {
+		if(mediaPlayer != null) {
+			mediaPlayer.stop();
+		}
+	}
+}
